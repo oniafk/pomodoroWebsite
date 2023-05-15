6ce16bc9b45d485ed9249ddb73d8ebee4804bc6f
Onimae <manufelipearias@gmail.com>
2023-05-15 18:21:34 -0500

    getting weater information by request to API and show current location by location API on browser

--- weaterAPI.js
+++ weaterAPI.js
@@ -4,6 +4,38 @@ const api = axios.create({
         'Content-Type': 'application/json;charset=utf-8',   
     },
     params: {
-        api_key: API_KEY,
+        appid: API_KEY,
     }
 });
+
+let currentPosition = {};
+
+function getPosition() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  }
+
+async function getWeather ({lat, lng}) {
+        const {data} = await api(`weather?lat=${lat}&lon=${lng}`);
+        console.log(data);    
+}
+
+async function showWeaterCurrentPosition() {
+    try {
+      currentPosition = await getPosition();      
+      setTimeout(() => {
+        if (currentPosition) {
+          const {latitude, longitude} = currentPosition.coords;
+          getWeather({lat: latitude, lng: longitude});
+        } else {
+          console.log('current position aviable');
+        }
+      }, 3000);
+    } catch (error) {
+      console.log('Error getting position:', error);
+      getWeather({lat: 20.7503, lng: -156.5003});
+    }
+  }
+  
+  showWeaterCurrentPosition();
